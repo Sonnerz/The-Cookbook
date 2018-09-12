0f6b166eedd105b7324345d963d1b323a0fc85d6
Sonnerz <Sonnerz@users.noreply.github.com>
2018-09-12 08:55:40 +0100

    Recreated requirements.txt. Configured my debug panel for this project. Prepped application for test deployment to Heroku

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
+import sys 
 from config import get_uri, get_dbs_name
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, session,current_app
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
@@ -11,11 +12,16 @@ app.config["MONGO_URI"] = get_uri()
 mongo = PyMongo(app)
 
 
+@app.context_processor
+def debug_on_off():
+    return dict(debug=app.debug)
+
+
 @app.route('/')
 @app.route('/get_test')
 def get_tasks():
     return render_template("index.html", test=mongo.db.test_collection.find())
 
-  
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
