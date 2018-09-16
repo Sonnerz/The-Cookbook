663ecb9db7f8e2a516d50507d7b41a3b5e7f4d34
Sonnerz <Sonnerz@users.noreply.github.com>
2018-09-16 18:40:20 +0100

    Added  dynamically add input functionality to Add Recipe form. Not fully tested

--- static/js/cookbook_custom_scripts.js
+++ static/js/cookbook_custom_scripts.js
@@ -62,7 +62,7 @@ function viewPanel() {
 }
 
 // add class to navbar link depending on the page displayed
-$(document).ready(function(){
+$(document).ready(function () {
   var current_path = $(location).attr('pathname');
   //console.log(current_path)
   if (current_path == "/profile") {
@@ -71,5 +71,56 @@ $(document).ready(function(){
   else if (current_path == "/add_recipe") {
     $("#add-nav-link").addClass("active-link");
   }
+
+  // add ingredients to add recipe form
+  // var max_fields = 6; 
+  // var x = 1; 
+  // $("#add_ingredient").on("click", function (e) {
+  //     e.preventDefault();
+  //     if (x < max_fields) { 
+  //         x++; 
+  //         $('#ingredients_input_list').append("<div>" +
+  //         "<input type='text' class='form-control' id='ingredients" + x + "'name='ingredients'" + x + ">" +
+  //         "<a href='' class='remove_ingredient_input' id='woo'>" +
+  //         "<i class='fa fa-minus-circle' aria-hidden='true'></i>" +
+  //         "</a>" + 
+  //         x + 
+  //         "</div>");
+  //     }
+  // });        
+  // $('#add_recipe_form').on('click', '.remove_ingredient_input', function () { 
+  //     $(this).parent().remove();
+  //     x--;
+  // })
+
+
+  var next = 1;
+  $("#add_ingredient").click(function (e) {
+    e.preventDefault();
+    var max_fields = 6;
+    var addto = "#ingredient" + next;
+    var addRemove = "#ingredient" + (next);
+    next = next + 1;
+    // if (next < max_fields) {
+      var newIn = '<input class="input form-control" id="ingredient' + next + '" name="ingredient' + next + '" type="text" placeholder="ingredient ' + next + '">';
+      var newInput = $(newIn);
+      var removeBtn = '<a href="#" id="remove' + (next - 1) + '" class="remove-me"><i class="fa fa-minus-circle" aria-hidden="true"></i></a>';
+      var removeButton = $(removeBtn);
+      $(addto).after(newInput);
+      $(addRemove).after(removeButton);
+      $("#ingredient" + next).attr('data-source', $(addto).attr('data-source'));
+      $("#count").val(next);
+    // }
+    
+
+    $('.remove-me').click(function (e) {
+      e.preventDefault();
+      var fieldNum = this.id.charAt(this.id.length - 1);
+      var fieldID = "#ingredient" + fieldNum;
+      $(this).remove();
+      $(fieldID).remove();
+    });
+  });
+
 });
 
