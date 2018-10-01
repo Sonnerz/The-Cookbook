74fad75577c700e414dc62b519cb629d385163b6
Sonnerz <Sonnerz@users.noreply.github.com>
2018-10-01 17:48:36 +0100

    Created json file of recipes Created stats.py Began creating dataframes and testing

--- stats.py
+++ stats.py
@@ -1,18 +1,43 @@
 # %matplotlib notebook
 import matplotlib.pyplot as plt
 import json
+import re
+import pandas
 
-RECIPES_DATA_PATH = 'recipes.json'
+RECIPES_DATA_PATH = 'static/data/recipes.json'
 
-results = []
-# with open(RECIPES_DATA_PATH) as recipes_file:
-with open('recipes.json') as recipes_file:
-    for recipe in recipes_file:
-        try:
-            status = json.loads(recipe)
-            results.append(status)
-        except ValueError:
-            pass
-print(len(results))
+def read_json(file_path):
+    results = []
+    with open(RECIPES_DATA_PATH) as recipes_file:
+        for recipes in recipes_file:
+            try:
+                recipe = json.loads(recipes)
+                results.append(recipe)
+            except ValueError:
+                pass
+        print(len(results))
+        return results
+
+
+def is_text_in_description(token, text):
+    token = token.lower()
+    text = ''.join(text).lower()
+    match = re.search(token, text)
+    if match:
+        return True
+    return False
+
+
+results = read_json(RECIPES_DATA_PATH)
+recipesDataFrame = pandas.DataFrame()
+recipesDataFrame['description'] = [recipe['description'] for recipe in results]
+
+recipesDataFrame['tarts'] = recipesDataFrame['description'].apply(lambda recipe: is_text_in_description('tarts', recipe))
+
+print(recipesDataFrame['tarts'].value_counts()[True])
+
+
+
+
 
 
